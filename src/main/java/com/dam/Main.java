87048d0e9e9b9d1d750c655bfaa293d7b2a70e0d
JuarezDAM <5865909@alu.murciaeduca.es>
2025-03-17 23:08:57 +0100

    version 3

--- src/main/java/com/dam/Main.java
+++ src/main/java/com/dam/Main.java
@@ -2,6 +2,8 @@ package com.dam;
 
 public class Main {
 	public static void main(String[] args) {
+
 		System.out.println("Hola mundo");
+		System.out.println("Hola mundo desde otra cuenta");
 	}
 }
